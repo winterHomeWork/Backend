09d26d0b51bc685e157778984fc0cf966c54f97d
도경 <dokyungkim2926@gmail.com>
2024-02-07 15:20:10 +0900

    ㅈㅂ

--- src/main/java/com/anys34/homework/infra/feign/client/KaKaoClient.java
+++ src/main/java/com/anys34/homework/infra/feign/client/KaKaoClient.java
@@ -2,16 +2,17 @@ package com.anys34.homework.infra.feign.client;
 
 import com.anys34.homework.domain.user.presentation.dto.KakaoInfo;
 import com.anys34.homework.infra.feign.client.dto.KaKaoToken;
+import com.anys34.homework.infra.feign.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "KaKaoClient", url = "https://kauth.kakao.com")
+@FeignClient(name = "KaKaoClient", url = "https://kauth.kakao.com", configuration = FeignConfig.class)
 public interface KaKaoClient {
 
-    @GetMapping("/v2/user/me")
+    @PostMapping("/v2/user/me")
     KakaoInfo getInfo(@RequestHeader("Authorization") String accessToken);
 
     @GetMapping("/oauth/authorize")
