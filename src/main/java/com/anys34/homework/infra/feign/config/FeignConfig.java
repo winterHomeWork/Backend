797da98837fc8d9638461076866de4c271863d75
도경 <dokyungkim2926@gmail.com>
2024-02-07 16:11:42 +0900

    .

--- src/main/java/com/anys34/homework/infra/feign/config/FeignConfig.java
+++ src/main/java/com/anys34/homework/infra/feign/config/FeignConfig.java
@@ -1,13 +1,21 @@
 package com.anys34.homework.infra.feign.config;
 
 import feign.Client;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @EnableFeignClients(basePackages = {"com"})
 @Configuration
 public class FeignConfig {
+
+    @Bean
+    public HttpMessageConverters customConverters() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        return new HttpMessageConverters(converter);
+    }
     @Bean
     public Client feignClient() {
         return new Client.Default(null, null);
