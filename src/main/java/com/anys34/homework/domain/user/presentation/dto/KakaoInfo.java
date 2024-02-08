62b8633d0ac622c5ce914157f390bf2f009bf61d
도경 <dokyungkim2926@gmail.com>
2024-02-08 23:01:35 +0900

    delete :: json naming

--- src/main/java/com/anys34/homework/domain/user/presentation/dto/KakaoInfo.java
+++ src/main/java/com/anys34/homework/domain/user/presentation/dto/KakaoInfo.java
@@ -1,13 +1,11 @@
 package com.anys34.homework.domain.user.presentation.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoInfo {
     private String email;
     private String name;
