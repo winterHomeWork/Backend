4c2102136b18f18224a1118d5855f92038710875
anys34 <anyeseong34@gmail.com>
2024-02-10 18:27:34 +0900

    Refactor :: Kakao Feign

--- src/main/java/com/anys34/homework/infra/feign/client/KaKaoClient.java
+++ src/main/java/com/anys34/homework/infra/feign/client/KaKaoClient.java
@@ -1,16 +1,11 @@
 package com.anys34.homework.infra.feign.client;
 
-import com.anys34.homework.domain.user.presentation.dto.KakaoInfo;
-import com.anys34.homework.infra.feign.client.dto.KaKaoToken;
-import com.anys34.homework.infra.feign.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "KaKaoClient", url = "https://kauth.kakao.com", configuration = FeignConfig.class)
+@FeignClient(name = "KaKaoClient", url = "https://kapi.kakao.com")
 public interface KaKaoClient {
-    @PostMapping(value = "/v2/user/me", produces = "application/json")
-    KakaoInfo getInfo(@RequestHeader("Authorization") String accessToken);
+    @GetMapping(value = "/v2/user/me")
+    String getInfo(@RequestHeader("Authorization") String accessToken);
 }
