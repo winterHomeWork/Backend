787d37c722817a4d41d3f3ab2013bbc3b734cd05
anys34 <anyeseong34@gmail.com>
2024-02-10 18:28:25 +0900

    Refactor :: KakaoService

--- src/main/java/com/anys34/homework/domain/user/service/KakaoService.java
+++ src/main/java/com/anys34/homework/domain/user/service/KakaoService.java
@@ -7,6 +7,9 @@ import com.anys34.homework.global.security.jwt.JwtTokenProvider;
 import com.anys34.homework.global.security.jwt.dto.TokenResponse;
 import com.anys34.homework.infra.feign.client.KaKaoClient;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,19 +21,29 @@ public class KakaoService {
     private final UserRepository userRepository;
 
     @Transactional
-    public TokenResponse save(String accessToken) {
-        KakaoInfo kaKaoInfo = kaKaoClient.getInfo(accessToken);
-            if (!userRepository.existsByEmail(kaKaoInfo.getEmail())) {
-                User user = User.builder()
-                        .email(kaKaoInfo.getEmail())
-                        .name(kaKaoInfo.getName())
-                        .build();
+    public TokenResponse save(String accessToken) throws ParseException {
+        String info = kaKaoClient.getInfo("Bearer " + accessToken);
 
-                userRepository.save(user);
-            }
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(info);
 
-            return jwtTokenProvider.getToken(kaKaoInfo.getEmail());
+        JSONObject kakao_account = (JSONObject) jsonObject.get("kakao_account");
+        String email = kakao_account.get("email").toString();
+
+        JSONObject properties = (JSONObject) jsonObject.get("properties");
+        String nickname = properties.get("nickname").toString();
+
+        KakaoInfo kakaoInfo = new KakaoInfo(email, nickname);
+
+        if (!userRepository.existsByEmail(kakaoInfo.getEmail())) {
+            User user = User.builder()
+                    .email(kakaoInfo.getEmail())
+                    .name(kakaoInfo.getName())
+                    .build();
+            userRepository.save(user);
+        }
+
+        return jwtTokenProvider.getToken(kakaoInfo.getEmail());
     }
-
 }
 
