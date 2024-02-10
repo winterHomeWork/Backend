bf5bd695c7d08a271596266bc9ec40a13e301387
anys34 <anyeseong34@gmail.com>
2024-02-10 18:25:10 +0900

    Fix :: FeignConfig

--- src/main/java/com/anys34/homework/infra/feign/config/FeignConfig.java
+++ src/main/java/com/anys34/homework/infra/feign/config/FeignConfig.java
@@ -1,17 +1,9 @@
 package com.anys34.homework.infra.feign.config;
 
-import feign.Client;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
-@EnableFeignClients(basePackages = {"com"})
 @Configuration
+@EnableFeignClients(basePackages = {"com.anys34.homework.infra.feign.client"})
 public class FeignConfig {
-    @Bean
-    public Client feignClient() {
-        return new Client.Default(null, null);
-    }
 }
