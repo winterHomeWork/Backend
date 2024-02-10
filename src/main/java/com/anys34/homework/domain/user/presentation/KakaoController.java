60c6ba7fd1599e4d683cf481420f2617ea8bd60d
anys34 <anyeseong34@gmail.com>
2024-02-10 18:28:37 +0900

    Refactor :: Kakao Controller

--- src/main/java/com/anys34/homework/domain/user/presentation/KakaoController.java
+++ src/main/java/com/anys34/homework/domain/user/presentation/KakaoController.java
@@ -5,12 +5,13 @@ import com.anys34.homework.global.security.jwt.dto.TokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "kaka login")
+@Tag(name = "kakao login")
 @RestController
 @RequestMapping("/kakao")
 @RequiredArgsConstructor
@@ -19,7 +20,7 @@ public class KakaoController {
 
     @Operation(summary = "카카오 정보 불러와서 데이터 저장")
     @GetMapping("/save")
-    public TokenResponse save(@RequestParam(name = "access_token") String token) {
+    public TokenResponse save(@RequestParam(name = "access_token") String token) throws ParseException {
         return kakaoService.save(token);
     }
 
