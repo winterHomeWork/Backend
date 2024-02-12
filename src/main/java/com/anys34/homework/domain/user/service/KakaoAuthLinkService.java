447a969f8467dc84691ca99d3fb53ed2c6023025
도경 <dokyungkim2926@gmail.com>
2024-02-12 23:54:35 +0900

    line

--- src/main/java/com/anys34/homework/domain/user/service/KakaoAuthLinkService.java
+++ src/main/java/com/anys34/homework/domain/user/service/KakaoAuthLinkService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class KakaoAuthLinkService {
+    
     private static final String QUERY_STRING = "?client_id=%s&" +
             "redirect_uri=%s&response_type=code";
     private final AuthProperties authProperties;
